07e01d8a54ac3c2cb2f89a2b1a19632ec59ee322
Will Ondrik <wondrik@my.bcit.ca>
2024-08-08 05:37:07 -0700

    Fixed formatting.

--- backend/src/abstract/abstract_controller.py
+++ backend/src/abstract/abstract_controller.py
@@ -7,7 +7,6 @@ class Abstract_Controller(ABC):
         self.base_path = base_path
         self.blueprint = Blueprint(base_path, __name__, url_prefix=f'/{base_path}')
 
-
     @abstractmethod
     def initialize_routes(self):
         """
