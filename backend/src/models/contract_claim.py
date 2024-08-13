9536c823917887d6f4e367e889e16ce3ebeb8d5c
Will Ondrik <wondrik@my.bcit.ca>
2024-08-12 19:15:33 -0700

    Created the ContractClaim model using SQLAlchemy ORM.

---
+++ backend/src/models/contract_claim.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Date, Float, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+# create base class
+Base = declarative_base()
+
+
+"""
+ORM model for the ContractClaim table.
+
+This class represents contract expenses (contract claims) for a Canadian Member of Parliament. 
+Each instance of this class corresponds with a row in the table, with attributes that represent the columns in the table.
+"""
+
+class ContractClaim(Base):
+    __tablename__ = 'ContractClaim'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    expense_id = Column('expenseId', Integer, ForeignKey('Expense.id'), nullable=False)
+    member_id = Column('memberId', Integer, ForeignKey('MembersOfParliament.id'), nullable=False)
+    supplier = Column(String(255), nullable=False)
+    date = Column(Date, nullable=False)
+    purpose = Column(Text, nullable=False)
+    total_cost = Column('totalCost', Float, nullable=False)
+
+
+    # defines a relationship to the Expense model, allowing access to the related Expense object via 'expense'
+    expense = relationship('Expense', back_populates='travel_claim')
+
+    # defines a relationship to the MembersOfParliament model, allowing access to the related MP object via 'member'
+    member = relationship('MembersOfParliament', back_populates='travel_claim')
+
+
+    def __repr__(self):
+        return (
+            f'<ContractClaim(id={self.id}, expense_id={self.expense_id}, member_id={self.member_id}, '
+            f'supplier={self.supplier}, date={self.date}, purpose={self.purpose}, total_cost={self.total_cost})>'
+        )
